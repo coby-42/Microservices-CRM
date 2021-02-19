553524228d4d8f329bb05117ba5c541a570aee46
coby-42 <77333935+coby-42@users.noreply.github.com>
2021-02-19 15:39:28 +0100

    init + eureka server

---
+++ eureka/src/test/java/com/ironhack/cmr/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.cmr.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
